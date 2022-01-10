c7c54aba55307add516c1c294715149ac0810953
jakesweis <jakesweis@gmail.com>
2022-01-10 14:05:02 -0600

    sign in and sign up

--- src/App.js
+++ src/App.js
@@ -4,6 +4,9 @@ import Navbar from './components/Navbar';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages';
 import Watchlists from './pages/watchlists';
+import Settings from './pages/settings';
+import Signup from './pages/sign-up';
+import Signin from './pages/sign-in';
 
 function App() {
   return (
@@ -12,6 +15,9 @@ function App() {
         <Routes>
           <Route exact path='/' element={<Home />} />
           <Route path='/watchlists' element={<Watchlists />} />
+          <Route path='/settings' element={<Settings />} />
+          <Route path='/sign-up' element={<Signup />} />
+          <Route path='/sign-in' element={<Signin />} />
         </Routes>
       </BrowserRouter>
   );
